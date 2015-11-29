fd885d2f2baed2ce7037dbd3e4f7e5e321ef3364
Moritz <moritzrsch@outlook.com>
2015-11-29 14:45:21 +0100

    Initial

---
+++ BlogCode_Band_Android/Band.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Microsoft.Band.Portable;
+
+namespace BlogCode_Band_Android {
+    class Band {
+        private static Band BandDevice;
+        public BandClient BandClient { get; set; }
+        public String Name { get; set; }
+
+        private Band() {
+
+        }
+
+        private async void PairBand() {
+            var bandClientManager = BandClientManager.Instance
+            var bandsFound = await bandClientManager.GetPairedBandsAsync();
+
+            if (bandsFound != null && bandsFound.Count() == 1)
+            {
+                var bandDevice = bandsFound.FirstOrDefault();
+                BandDevice.BandClient = await bandClientManager.ConnectAsync(bandDevice);
+                BandDevice.Name = bandDevice.Name;
+            }
+        }
+
+        public static Band Instance {
+            get {
+                if (BandDevice == null)
+                {
+                    BandDevice = new Band();
+                    BandDevice.PairBand();
+                }
+                return BandDevice;
+            }
+        }
+    }
+}
---
+++ BlogCode_Band_Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.Band.Portable;
+
+namespace BlogCode_Band_Android {
+    [Activity(Label = "BlogCode_Band_Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate
+            {
+                var myBand = Band.Instance;
+
+                button.Text = string.Format("Connected to {0}!", myBand.Name);
+            };
+        }
+    }
+}
+
